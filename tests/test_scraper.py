811c8f7cc4db81c422ee7ef0dcf03cb33bfbd3df
tibtiq <29826331+tibtiq@users.noreply.github.com>
2026-07-22 05:53:26 +0000

    unused imports

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,6 +1,5 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
-import pytest
 from bs4 import BeautifulSoup
 
 from wikipediascraper.scraper import digest_page, scrape_wiki_page
